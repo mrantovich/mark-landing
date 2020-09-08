aad4473095f6546596c2ccad5017336cfcf19141
Mikhail Rantovich <mrantovich@gmail.com>
2020-09-08 21:29:31 +0700

    Вертикальное меню для небольших экранов

--- public/js/script.js
+++ public/js/script.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
 	$('.header__burger').on('click', function(){
 		$(this).toggleClass('_active');
 		$('.header__menu').toggleClass('_active');
+		$('.header__socials').toggleClass('_active');
 	});
 
 });
